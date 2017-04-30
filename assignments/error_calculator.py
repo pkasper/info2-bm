0de9abbf3f14f33d3e31163ceae9b71c1389ca8b
patrick_kasper <prepare4stanford!>
2017-04-30 16:53:19 -0700

    update

--- assignments/error_calculator.py
+++ assignments/error_calculator.py
@@ -5,8 +5,12 @@ class ErrorCalculator(metaclass=abc.ABCMeta):
     This is an abstract class and thus, concrete implementations of the 
     error calculation methods have to be defined in the child classes.
     
+    IMPORTANT: The predefined lengthcheck was changed. Both this and the
+               old version will be accepted for submissions!
+    
     Args:
-      data (list): The series of true/test data. Can be of np.array().
+      data (list): The series of true/test data. Can be of np.array(). 
+                   This is the series of values you want to test against.
 
     Attributes:
       _data (str): The stored list of of true/test data. Only to be set
@@ -40,12 +44,12 @@ class ErrorCalculator(metaclass=abc.ABCMeta):
         TODO: Implement typechecks for y
         
         Args:
-          y: The series to test with
+          y: The series to test with (against the values in self._data)
 
         Returns:
           The return value. The calculated error of series y on self_data
         """
         if self._data is None:
             raise ValueError("Data is not set")    
-        if len(y) != len(self._data[1]):
+        if len(y) != len(self._data):
             raise ValueError("Series are not of identical length")
